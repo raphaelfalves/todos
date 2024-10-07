2a05e5e1669f54bda677d01fed864812dacaecf6
raphaelfalves <raphaelfranciscodev@gmail.com>
2024-10-06 21:31:28 -0300

    Fixes AB#77, fixes AB#78, fixes AB#83 84, fixes AB#85, fixes AB#86, fixes AB#87

--- ToDosProject.Tests/WebTests.cs
+++ ToDosProject.Tests/WebTests.cs
@@ -1,28 +1,115 @@
+using Bunit;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.DataProtection;
+using ToDosProject.Domain;
+using ToDosProject.Web;
+using ToDosProject.Web.Components.Pages;
+
 namespace ToDosProject.Tests;
 
-public class WebTests
+public class WebTests : TestContext, IDisposable
 {
-    [Fact]
-    public async Task GetWebResourceRootReturnsOkStatusCode()
+    private readonly DistributedApplication app = null!;
+    private readonly HttpClient httpClientWeb = null!;
+    private readonly HttpClient httpClientApi = null!;
+    public WebTests()
     {
-        // Arrange
-        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.ToDosProject_AppHost>();
+        var appHost = DistributedApplicationTestingBuilder.CreateAsync<Projects.ToDosProject_AppHost>().Result;
         appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
         {
             clientBuilder.AddStandardResilienceHandler();
         });
-        // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
 
-        await using var app = await appHost.BuildAsync();
+        app = appHost.BuildAsync().Result;
         var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-        await app.StartAsync();
+        app.StartAsync().Wait();
 
+        resourceNotificationService.WaitForResourceAsync(AppConfiguration.DATABASE, KnownResourceStates.Running).Wait();
+        resourceNotificationService.WaitForResourceAsync(AppConfiguration.CACHE, KnownResourceStates.Running).Wait();
+        resourceNotificationService.WaitForResourceAsync(AppConfiguration.API, KnownResourceStates.Running).Wait();
+        resourceNotificationService.WaitForResourceAsync(AppConfiguration.WEB, KnownResourceStates.Running).Wait();
+        resourceNotificationService.WaitForResourceAsync(AppConfiguration.MIGRATION, KnownResourceStates.Finished).Wait();
+
+        httpClientWeb = app.CreateHttpClient(AppConfiguration.WEB);
+        httpClientApi = app.CreateHttpClient(AppConfiguration.API);
+
+
+        ApiServiceClient apiServiceClient = new(httpClientApi);
+
+        Services.AddSingleton(apiServiceClient);
+    }
+
+    [Fact]
+    public async Task GetWebResourceRootReturnsOkStatusCode()
+    {
         // Act
-        var httpClient = app.CreateHttpClient("webfrontend");
-        await resourceNotificationService.WaitForResourceAsync("webfrontend", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-        var response = await httpClient.GetAsync("/");
+        var response = await httpClientWeb.GetAsync("/");
 
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetWebResourceTodosReturnsOkStatusCode()
+    {
+        var cut = RenderComponent<ToDoPage>();
+
+        var p = cut.Find("#p-loading");
+
+        Assert.True(cut.Instance.toDos == null || p.InnerHtml == "<em>Loading...</em>");
+
+        cut.WaitForElement("#input-createtodo", TimeSpan.FromMinutes(1));
+
+        var input = cut.Find("#input-createtodo");
+
+        input.Change("Nova tarefa");    
+
+        await cut.InvokeAsync(() => input.KeyUp(new KeyboardEventArgs { Key = "Enter"}));
+        
+        cut.WaitForElement("#tr-0", TimeSpan.FromSeconds(30));
+        
+        var inputTitle = cut.Find("#input-title-0");
+
+        inputTitle.Change("Nova tarefa 1");    
+
+        var checkbox = cut.Find("#input-isconcluded-0");
+
+        checkbox.Click();
+
+        cut.WaitForState(() => cut.Instance.toDos![0].IsConcluded == true,TimeSpan.FromSeconds(10));
+
+        cut = RenderComponent<ToDoPage>();
+
+        cut.WaitForState(() => cut?.Instance?.toDos != null, TimeSpan.FromSeconds(30));
+
+        Assert.Equal("Nova tarefa 1", cut.Instance.toDos![0].Title);
+        Assert.True(cut.Instance.toDos![0].IsConcluded);
+
+        cut = RenderComponent<ToDoPage>();
+
+        cut.WaitForState(() => cut?.Instance?.toDos != null, TimeSpan.FromSeconds(30));
+
+        checkbox = cut.Find("#input-isconcluded-0");
+
+        checkbox.Click();
+
+        cut.WaitForState(() => cut.Instance.toDos![0].IsConcluded == false, TimeSpan.FromSeconds(10));
+
+        cut = RenderComponent<ToDoPage>();
+
+        cut.WaitForState(() => cut?.Instance?.toDos != null, TimeSpan.FromSeconds(30));
+
+        Assert.False(cut.Instance.toDos![0].IsConcluded);
+
+        checkbox = cut.Find("#input-delete-0");
+
+        checkbox.Click();
+
+        cut.WaitForState(() => cut?.Instance?.toDos.Count() == 0, TimeSpan.FromSeconds(30));
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        app.StopAsync().Wait();
     }
 }
---
+++ ToDosProject.Web/ApiServiceClient.cs
@@ -0,0 +1,56 @@
+using ToDosProject.Domain;
+
+namespace ToDosProject.Web;
+
+public class ApiServiceClient(HttpClient httpClient)
+{
+    public async Task<ToDo[]> GetToDosAsync(CancellationToken cancellationToken = default)
+    {
+        IEnumerable<ToDo>? toDos = [];
+
+        var response = await httpClient.GetAsync("/todoitems", cancellationToken);
+
+        if(response.IsSuccessStatusCode)
+            toDos = await response.Content.ReadFromJsonAsync<IEnumerable<ToDo>>(cancellationToken);
+
+        return toDos!.ToArray();
+    }
+
+    public async Task<ToDo?> CreateToDosAsync(ToDo? toDo, CancellationToken cancellationToken = default)
+    {
+        var response = await httpClient.PostAsJsonAsync("/todoitems", toDo, cancellationToken);
+
+        if(response.IsSuccessStatusCode)
+            return  await response.Content.ReadFromJsonAsync<ToDo>(cancellationToken);
+
+        return null;
+    }
+
+    public async Task<bool> DeleteToDoAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var response = await httpClient.DeleteAsync($"/todoitems/{id}", cancellationToken);
+
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> UpdateToDoAsync(ToDo toDo, CancellationToken cancellationToken = default)
+    {
+        var response = await httpClient.PutAsJsonAsync($"/todoitems/{toDo.Id}",toDo, cancellationToken);
+
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> UnconcludeToDoAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var response = await httpClient.PostAsJsonAsync($"/todoitems/Unconclude/{id}", new StringContent(""), cancellationToken);
+
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> ConcludeToDoAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var response = await httpClient.PostAsync($"/todoitems/Conclude/{id}",new StringContent(""), cancellationToken);
+
+        return response.IsSuccessStatusCode;
+    }
+}
