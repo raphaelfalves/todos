5c7a3638b373ceb6e3bfd40b227c8f45e022f5d2
raphaelfalves <raphaelfranciscodev@gmail.com>
2024-10-10 07:46:37 -0300

    Organization the project

--- ToDosProject.Domain/Constants/AppConfiguration.cs
+++ ToDosProject.Domain/Constants/AppConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToDosProject.Domain
+﻿namespace ToDosProject.Domain.Constants
 {
     public static class AppConfiguration
     {
---
+++ ToDosProject.Infraestructure/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDosProject.Domain.Entities;
+
+namespace ToDosProject.Infraestructure.Context
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> opt) : IdentityDbContext<User>(opt)
+    {
+        public DbSet<ToDo> ToDo { get; set; }
+    }
+}
